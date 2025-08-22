fbf4e77f31e22fe46ecf2e25a31119e22428238b
mikobrno <ingkost@gmail.com>
2025-08-23 00:06:51 +0200

    Fix test import to named Select

--- src/components/ui/__tests__/select.test.tsx
+++ src/components/ui/__tests__/select.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import Select from '../select';
+import { Select } from '../select';
 
 test('renders Select component', () => {
   const options = [
